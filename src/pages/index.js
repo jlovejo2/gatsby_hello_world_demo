be8ad0700a3e36a8e06b938b1c2817c47a66956c
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-14 12:47:09 -0500

    added contact page and linked to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
-      <h1>TEsting testing testing!</h1>
+      <Link to="/contact">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
       <p>what a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
 }
